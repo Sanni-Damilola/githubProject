9dbadb1c31ccda3e749074e1e2c9f22ab5275b12
Sanni-Damilola <sannifortune11@gmail.com>
2023-03-08 13:57:28 +0100

    update

--- src/Components/Leader.jsx
+++ src/Components/Leader.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Leader = () => {
   return (
-    <div>
+    <Container>
       Leader
-    </div>
+    </Container>
   )
 }
 
 export default Leader
+
+
+const Container = styled.div`
+width: 100%;
+height: 100vh;
+background-color: red;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 30px;
+font-weight: bold;
+`
