2e1610992f0386b2c883bbf63fd9ebd0ac2e3b81
victor-okereke <okerekev324@gmail.com>
2023-03-08 07:56:12 -0800

    more hero contents

--- src/Components/Victor.jsx
+++ src/Components/Victor.jsx
@@ -12,6 +12,10 @@ const Victor = () => {
           </Header>
           <Hero>
             <MainPart>The no-code landing page of your dreams</MainPart>
+            <SecondPart>
+              Rustle up a beautiful landing page in minutes, and rake in the
+              leads like a boss.
+            </SecondPart>
           </Hero>
         </Wrapper>
       </Container>
@@ -34,7 +38,16 @@ const Wrapper = styled.div`
 const Header = styled.div`
   width: 100%;
   display: flex;
+  justify-content: space-between;
   height: 70px;
+  font-size: 30px;
+  button {
+    width: 100px;
+    height: 40px;
+    border-radius: 5px;
+    background-color: black;
+    color: white;
+  }
 `;
 const Logo = styled.div`
   font-family: cursive;
@@ -45,8 +58,19 @@ const Hero = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `;
 const MainPart = styled.div`
+  width: 60%;
   display: flex;
   text-align: center;
+  font-size: 60px;
+`;
+const SecondPart = styled.div`
+  width: 50%;
+  display: flex;
+  text-align: center;
+  font-size: 25px;
+  margin-top: 50px;
+  font-weight: lighter;
 `;
