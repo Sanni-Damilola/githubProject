b057e8b25de1e983c8a070003f80bb0759850005
Sanni-Damilola <sannifortune11@gmail.com>
2023-03-08 15:53:44 +0100

    uodate

--- src/Components/Leader.jsx
+++ src/Components/Leader.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Leader = () => {
   return (
-    <div>Leader</div>
+    <Container>
+        <Right>
+            <Wrap>
+                <span></span>
+                <button>
+                    show me
+                </button>
+            </Wrap>
+        </Right>
+        <Left />
+    </Container>
   )
 }
 
 export default Leader
+
+const Container = styled.div``
+const Right = styled.div``;
+const Wrap = styled.div``;
+const Left = styled.img``;
+
