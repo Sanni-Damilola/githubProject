004fa84e1cd8df14620cf8a1546203d9a61c5eea
victor-okereke <okerekev324@gmail.com>
2023-03-08 07:26:37 -0800

    structure the divs

--- src/Components/Victor.jsx
+++ src/Components/Victor.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 const Victor = () => {
   return (
-    <div>Victor</div>
-  )
-}
+    <div>
+      <Container>
+        <Wrapper>
+          <Header>
+            <Logo>Typeform</Logo>
+            <button>Sign In</button>
+          </Header>
+          <Hero></Hero>
+        </Wrapper>
+      </Container>
+    </div>
+  );
+};
 
-export default Victor
+export default Victor;
+
+const Container = styled.div`
+width: 100vw;
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+const Wrapper = styled.div``;
+const Header = styled.div`
+width: 100%;
+
+`;
+const Logo = styled.div`
+font-family: cursive;
+`;
+const Hero = styled.div``;
