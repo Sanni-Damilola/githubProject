973aa6f68a0ac2fd262c0a59a15c6e4b8204821f
Phinatech <ugbojudithchinemerem@gmil.com>
2023-03-08 18:01:49 +0100

    judith profile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import logo from './logo.svg';
-import './App.css';
-import AllRoutes from './Components/AllRoutes';
-
+import "./App.css";
+import AllRoutes from "./Components/AllRoutes";
+import Judith from "./Components/Judith";
 
 function App() {
   return (
     <div>
-   <AllRoutes />
+      <AllRoutes />
+      <judith />
     </div>
   );
 }
